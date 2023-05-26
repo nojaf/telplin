da805e4a6e9639c0fa6c653121f627b4c4afbad9
nojaf <florian.verdonck@outlook.com>
2023-05-26 12:11:29 +0200

    Add namespace to Assembly.GetExecutingAssembly()

--- tool/server/Lambda.fs
+++ tool/server/Lambda.fs
@@ -72,7 +72,11 @@ module Encoding =
 let resolvedAssemblies =
     let dir =
 #if RELEASE
-        Path.Combine (FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, "ref")
+        Path.Combine (
+            FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location)
+                .Directory.FullName,
+            "ref"
+        )
 #else
         Path.Combine (__SOURCE_DIRECTORY__, "..", "..", "reference")
 #endif
