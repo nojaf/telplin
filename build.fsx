d07166b6863617d75c8684d97ad1d35e3b9e55ff
nojaf <florian.verdonck@outlook.com>
2023-06-09 09:02:04 +0200

    Use a local compiler.

--- build.fsx
+++ build.fsx
@@ -1,13 +1,52 @@
 #r "nuget: Fun.Build, 0.3.8"
 #r "nuget: Fake.IO.FileSystem, 6.0.0"
+#r "nuget: CliWrap, 3.6.0"
 
 open System
 open System.IO
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fun.Build
+open CliWrap
 
 let apiKey = Environment.GetEnvironmentVariable "TELPLIN_NUGET_KEY"
+let fsharpCompiler = Path.Combine (__SOURCE_DIRECTORY__, ".fsharp") |> DirectoryInfo
+let fsharpCompilerCommit = "1e5b5e4f52b8c09015d0420c2adbcc33a73456c5" // Don't consider seq`1 prefix in NicePrint.
+
+let runCommand (file : string) (arguments : string) =
+    task {
+        let! _result =
+            Cli
+                .Wrap(file)
+                .WithWorkingDirectory(fsharpCompiler.FullName)
+                .WithArguments(arguments)
+                .WithStandardOutputPipe(PipeTarget.ToDelegate (printfn "%s"))
+                .ExecuteAsync()
+                .Task
+
+        return ()
+    }
+
+let git = runCommand "git"
+let dotnet = runCommand "dotnet"
+
+pipeline "Init" {
+    workingDir __SOURCE_DIRECTORY__
+    stage "clone local compiler" {
+        run (fun _ ->
+            task {
+                if not fsharpCompiler.Exists then
+                    fsharpCompiler.Create ()
+                    do! git "init"
+                    do! git "remote add origin https://github.com/dotnet/fsharp.git"
+                    do! git "fetch origin"
+                do! git $"reset --hard {fsharpCompilerCommit}"
+                do! dotnet "build src/Compiler/FSharp.Compiler.Service.fsproj /p:BUILDING_USING_DOTNET=true"
+            }
+        )
+    }
+    runIfOnlySpecified true
+}
 
 pipeline "Build" {
     workingDir __SOURCE_DIRECTORY__
@@ -29,7 +68,13 @@ pipeline "Build" {
         run "dotnet tool restore"
         run "dotnet fantomas . --check"
     }
+    stage "restore" { run "dotnet restore" }
     stage "build" {
+        run (fun _ ->
+            task {
+                do! dotnet "build -c Release src/Compiler/FSharp.Compiler.Service.fsproj /p:BUILDING_USING_DOTNET=true"
+            }
+        )
         run "dotnet restore ./telplin.sln"
         run "dotnet build --no-restore -c Release ./telplin.sln"
     }
