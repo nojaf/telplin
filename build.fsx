1b4355fa4350bee41d0fed93526eb99786319981
Florian Verdonck <florian.verdonck@outlook.com>
2023-06-20 19:54:26 +0200

    Update dotnet7 prerelease nuget feed. (#93)

--- build.fsx
+++ build.fsx
@@ -1,68 +1,13 @@
 #r "nuget: Fun.Build, 0.3.8"
 #r "nuget: Fake.IO.FileSystem, 6.0.0"
-#r "nuget: CliWrap, 3.6.0"
 
 open System
 open System.IO
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fun.Build
-open CliWrap
 
 let apiKey = Environment.GetEnvironmentVariable "TELPLIN_NUGET_KEY"
-let fsharpCompiler = Path.Combine (__SOURCE_DIRECTORY__, ".fsharp") |> DirectoryInfo
-let fsharpCompilerRemote = "https://github.com/dotnet/fsharp.git"
-let fsharpCompilerCommit = "fd051378c015490d5a8a7a3424f8fc260378bffd" // See https://github.com/dotnet/fsharp/pull/15435
-
-let runCommand (file : string) (arguments : string) =
-    task {
-        let! _result =
-            Cli
-                .Wrap(file)
-                .WithWorkingDirectory(fsharpCompiler.FullName)
-                .WithArguments(arguments)
-                .WithStandardOutputPipe(PipeTarget.ToDelegate (printfn "%s"))
-                .WithStandardErrorPipe(PipeTarget.ToDelegate (printfn "%s"))
-                .ExecuteAsync()
-                .Task
-
-        return ()
-    }
-
-let git = runCommand "git"
-let dotnet = runCommand "dotnet"
-
-pipeline "Init" {
-    workingDir __SOURCE_DIRECTORY__
-    stage "clone local compiler" {
-        run (fun _ ->
-            task {
-                if not fsharpCompiler.Exists then
-                    fsharpCompiler.Create ()
-                    do! git "init"
-                    do! git $"remote add origin {fsharpCompilerRemote}"
-
-                do! git "fetch origin"
-                do! git $"reset --hard {fsharpCompilerCommit}"
-
-                let configuration =
-                    let idx = fsi.CommandLineArgs |> Array.tryFindIndex (fun arg -> arg = "-c")
-                    match idx with
-                    | None -> "Debug"
-                    | Some idx ->
-                        if idx + 1 < fsi.CommandLineArgs.Length then
-                            fsi.CommandLineArgs.[idx + 1]
-                        else
-                            "Debug"
-
-                do!
-                    dotnet
-                        $"build -c {configuration} src/Compiler/FSharp.Compiler.Service.fsproj /p:BUILDING_USING_DOTNET=true"
-            }
-        )
-    }
-    runIfOnlySpecified true
-}
 
 pipeline "Build" {
     workingDir __SOURCE_DIRECTORY__
@@ -86,11 +31,6 @@ pipeline "Build" {
     }
     stage "restore" { run "dotnet restore" }
     stage "build" {
-        run (fun _ ->
-            task {
-                do! dotnet "build -c Release src/Compiler/FSharp.Compiler.Service.fsproj /p:BUILDING_USING_DOTNET=true"
-            }
-        )
         run "dotnet restore ./telplin.sln"
         run "dotnet build --no-restore -c Release ./telplin.sln"
     }
