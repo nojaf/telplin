9c230530af3365de51de8956d3904429a13e393c
nojaf <florian.verdonck@outlook.com>
2023-11-22 13:50:18 +0100

    8.0.100 + fsdocs bump.

--- build.fsx
+++ build.fsx
@@ -1,45 +1,13 @@
-#r "nuget: Fun.Build, 1.0.3"
+#r "nuget: Fun.Build, 1.0.4"
 #r "nuget: Fake.IO.FileSystem, 6.0.0"
 
 open System
 open System.IO
-open System.Text.Json
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fun.Build
 
 let apiKey = Environment.GetEnvironmentVariable "TELPLIN_NUGET_KEY"
-
-/// Workaround for https://github.com/dotnet/sdk/issues/35989
-let restoreTools (ctx : Internal.StageContext) =
-    async {
-        let json = File.readAsString ".config/dotnet-tools.json"
-        let jsonDocument = JsonDocument.Parse (json)
-        let root = jsonDocument.RootElement
-        let tools = root.GetProperty ("tools")
-
-        let! installs =
-            tools.EnumerateObject ()
-            |> Seq.map (fun tool ->
-                let version = tool.Value.GetProperty("version").GetString ()
-                ctx.RunCommand $"dotnet tool install %s{tool.Name} --version %s{version}"
-            )
-            |> Async.Sequential
-
-        let failedInstalls =
-            installs
-            |> Array.tryPick (
-                function
-                | Ok _ -> None
-                | Error error -> Some error
-            )
-
-        match failedInstalls with
-        | None -> return 0
-        | Some error ->
-            printfn $"%s{error}"
-            return 1
-    }
 
 pipeline "Build" {
     workingDir __SOURCE_DIRECTORY__
@@ -58,7 +26,7 @@ pipeline "Build" {
         )
     }
     stage "lint" {
-        run restoreTools
+        run "dotnet tool restore"
         run "dotnet fantomas . --check"
     }
     stage "restore" { run "dotnet restore" }
@@ -89,7 +57,7 @@ pipeline "Build" {
 pipeline "Watch" {
     workingDir __SOURCE_DIRECTORY__
     stage "main" {
-        run restoreTools
+        run "dotnet tool restore"
         paralle
         run "dotnet fsi ./tool/client/dev-server.fsx"
         run "dotnet fsdocs watch --port 7890 --noapidocs"
