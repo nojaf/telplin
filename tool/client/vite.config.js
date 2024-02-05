8423a9d01f0e27fc8912498f02875cd2e0b297bb
nojaf <florian.verdonck@outlook.com>
2024-02-05 16:04:11 +0100

    Use fable plugin from npm

--- tool/client/vite.config.js
+++ tool/client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fable from "../../../vite-plugin-fable"
+import fable from "vite-plugin-fable"
 
 // https://vitejs.dev/config/
 export default defineConfig({
