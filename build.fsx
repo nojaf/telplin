dae28dacd95f70da5fc0d20623828f94c81604c0
Florian Verdonck <florian.verdonck@outlook.com>
2024-02-05 16:17:07 +0000

    Use Vite for dev tool (#202)
    * Replace custom dev-server with Vite
    
    * Update build script
    
    * Install Bun
    
    * Use fable plugin from npm
    
    * Update to design time build fix

--- build.fsx
+++ build.fsx
@@ -34,7 +34,11 @@ pipeline "Build" {
     stage "test" { run "dotnet test --no-restore --no-build -c Release -tl" }
     stage "pack" { run "dotnet pack ./src/Telplin/Telplin.fsproj -c Release -o bin -tl" }
     stage "docs" {
-        run "dotnet fsi ./tool/client/dev-server.fsx build"
+        stage "client" {
+            workingDir "tool/client"
+            run "bun i --frozen-lockfile"
+            run "bunx --bun vite build"
+        }
         run (fun _ -> Shell.copyRecursive "./tool/client/dist" "./docs" true |> ignore)
         run "dotnet fsdocs build --noapidocs"
     }
@@ -56,7 +60,16 @@ pipeline "Watch" {
     stage "main" {
         run "dotnet tool restore"
         paralle
-        run "dotnet fsi ./tool/client/dev-server.fsx"
+        stage "client" {
+            envVars [ "VITE_API_ROOT", "http://localhost:8906" ]
+            workingDir "tool/client"
+            run "bun i --frozen-lockfile"
+            run "bunx --bun vite"
+        }
+        stage "server" {
+            workingDir "tool/server"
+            run "dotnet watch"
+        }
         run "dotnet fsdocs watch --port 7890 --noapidocs"
     }
     runIfOnlySpecified true
---
+++ tool/client/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fable from "vite-plugin-fable"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({ jsxRuntime: 'classic' }), fable() ],
+})
