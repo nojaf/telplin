73b74343f38f0d8978d1009f29efe967fd010ace
nojaf <florian.verdonck@outlook.com>
2024-02-28 09:00:12 +0100

    Update vite-plugin-fable

--- tool/client/vite.config.js
+++ tool/client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fable from "vite-plugin-fable"
+import Inspect from "vite-plugin-inspect"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' }), fable() ],
+  plugins: [ Inspect(), fable(), react({ jsxRuntime: 'classic', include: /\.(jsx|fs)$/ }) ],
   base: '/telplin/'
 })
